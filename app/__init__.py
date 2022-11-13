0d2ee26253260055297d5f95f6662058f79b7626
Edwin Galeano <ed.galeano1@gmail.com>
2022-11-12 22:08:06 -0500

    Rest of the blueprints implementation

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 
 from .config import Config
+from .auth import auth
 
 def create_app():
     app = Flask(__name__) # Se crea nueva instancia de la clase Flask
     bootstrap = Bootstrap(app)
     app.config.update(**Config.config)
+    app.register_blueprint(auth)
 
     return app
